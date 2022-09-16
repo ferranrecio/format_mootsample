5460fbe119233527d87fabf8f046ae4aa6d5974c
Ferran Recio <ferran@moodle.com>
2022-09-16 10:06:27 +0200

    Step 05: create basic output and template structure
    Unlike output classes mustache templates cannot be extended. To allow
    template overriding the plugin must provide a basic structure of output
    classes and mustache files to allow both PHP and Ajax rendering.
    
    The basic structure is documented here:
    
    https://moodledev.io/docs/apis/plugintypes/format#creating-the-basic-output-structure

---
+++ classes/output/courseformat/content.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Output content for the format_mootsample plugin.
+ *
+ * @package   format_mootsample
+ * @copyright Year, You Name <your@email.address>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace format_mootsample\output\courseformat;
+
+use core_courseformat\output\local\content as content_base;
+
+class content extends content_base {
+
+    /**
+     * Returns the output class template path.
+     *
+     * This method redirects the default template when the course content is rendered.
+     */
+    public function get_template_name(\renderer_base $renderer): string {
+        return 'format_mootsample/local/content';
+    }
+}
